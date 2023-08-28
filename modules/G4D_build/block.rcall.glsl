425dfb6132e6be45ce05c8ac8ae08359674dbe36
olivier st-laurent <kzaurckichz@gmail.com>
2023-08-28 16:03:13 -0400

    Space and Aero

--- modules/G4D_build/block.rcall.glsl
+++ modules/G4D_build/block.rcall.glsl
@@ -10,4 +10,8 @@ void main() {
 	surface.color.a = (float(color.a & 0xf) + 1) / 16.0;
 	surface.roughness = float((color.a >> 4) & 0x7) / 7.0;
 	surface.metallic = float(color.a >> 7);
+	
+	vec3 posSeam = 1 - abs(fract(surface.localPosition * abs(1-surface.normal) / 3 + 0.5) * 2 - 1);
+	float seems = -pow(min(posSeam.x, min(posSeam.y, posSeam.z)), 0.125) * 0.05;
+	surface.color.rgb -= vec3(seems);
 }
